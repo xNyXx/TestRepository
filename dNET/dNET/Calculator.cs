d220a8e02c4c91c770a68bcef5914887d3348f29
Ildar Zalyaliev <vip.attano@inbox.com>
2020-09-15 14:09:54 +0300

    Calc

---
+++ dNET/dNET/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dNET
+{
+    static class Calculator
+    {
+        public static int GetNumber()
+        {
+            var a = int.Parse(Console.ReadLine());
+            return a;
+        }
+        public static int Calculate(int num1, string operation, int num2)
+        {
+            switch (operation)
+            {
+                case "+": return num1 + num2;
+                case "-": return num1 - num2;
+                case "/": return num1 / num2;
+                case "*": return num1 * num2;
+                default: throw new ArgumentException("invalid operation");
+            }
+        }
+    }
+}
---
+++ dNET/dNET/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Calculator.GetNumber();
+            var b = Calculator.GetNumber();
+            Console.WriteLine(Calculator.Calculate(a,"+",b));
+        }
+    }
+}
