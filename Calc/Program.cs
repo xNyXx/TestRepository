f5c18b28d1c6218ead50f207dec9709508af19b3
Ildar Zalyaliev <vip.attano@inbox.com>
2020-09-27 13:19:11 +0300

    Calc

---
+++ Calc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Calculator.GetNubmer();
+            var b = Calculator.GetNubmer();
+            Console.WriteLine(Calculator.Calculate(a, "+", b));
+        }
+    }
+    
+}
---
+++ Calc_Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System;
+using Calc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Sum_2plus3_5Returned()
+        {
+            Assert.AreEqual(Calculator.Calculate(2,"+",3),5);
+        }
+        
+        [TestMethod]
+        public void Sub_4minus3_1Returned()
+        {
+            Assert.AreEqual(Calculator.Calculate(4,"-",3),1);
+        }
+        [TestMethod]
+        public void Dev_5devision5_1Returned()
+        {
+            Assert.AreEqual(Calculator.Calculate( 5,"/",5),1);
+        }
+        [TestMethod]
+        public void Mult_5mult5_25Returned()
+        {
+            Assert.AreEqual(Calculator.Calculate( 5,"*",5),25);
+        }
+        [TestMethod]
+        public void WrongOperation_InvalidOperation_ExceptionReturned()
+        {
+            Assert.ThrowsException<ArgumentException>(() => Calculator.Calculate(5, "nope", 5),"Invalid operation");
+        }
+        [TestMethod]
+        public void Sum_0Devision_ExceptionReturned()
+        {
+            Assert.ThrowsException<Exception>(() => Calculator.Calculate(5, "/", 0),"Zero devision");
+        }
+    }
+}
