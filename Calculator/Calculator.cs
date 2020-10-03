2705de198659b04c53cbff32938012998ec5d9f9
xNyXx <vip.attano@gmail.com>
2020-10-03 14:09:11 +0300

    added coverage

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace dNET
+{
+    public static class Calculator
+    {
+        public static int GetNubmer()
+        {
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+        public static int Calculate(int num1, string operation, int num2)
+        {
+            switch (operation)
+            {
+                case "+" : return num1 + num2;
+                    break;
+                case "-" : return num1 - num2;
+                    break;
+                case "/" :
+                    if (num2 == 0) throw new Exception("Zero devision");
+                    return num1 / num2;
+                    break;
+                case "*" : return num1 * num2;
+                    break;
+                default: throw new ArgumentException("Invalid operation");
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dNET;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace dNET
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                var a = Calculator.GetNubmer();
+                var b = Calculator.GetNubmer();
+                Console.WriteLine(Calculator.Calculate(a, "+", b));
+            }
+        }
+    
+}
